de96b7e5bb73aae1695d434fcc21dbad5d72515e
Adar Hefer <adar.hefer@gmail.com>
2015-01-11 01:26:02 +0200

    Added ReadWriteLockBucketList to be used with the locking hash table implementation.

--- Homework 5/src/BucketList.java
+++ Homework 5/src/BucketList.java
@@ -11,7 +11,147 @@ public interface BucketList<T,K> {
   }
 }
 
+class LockingParallelBucketList<T,K> implements BucketList<T,K> {
+    size = 0;
+    LockingParallelBucketList<T,K>.Iterator<T,K> head;
+    ReadWriteLock lock;
 
+    public LockingParallelBucketList() {
+        this.head = null;
+        this.size = 0;
+        this.lock = new SimpleReadWriteLock();
+    }
+
+    public Iterator<T,K> getHead() {
+        return head;
+    }
+    
+    public Iterator<T,K> getItem(K key) {
+        LockingParallelBucketList<T,K>.Iterator<T,K> iterator = head;
+
+        while (iterator != null) {
+            if (iterator.key.equals(key)) {
+                return iterator;
+            } else {
+                iterator = iterator.next;
+            }            
+        }
+        return null;
+
+    }
+    public boolean contains(K key) {
+        LockingParallelBucketList<T,K>.Iterator<T,K> iterator;
+        
+        lockRead();
+        try {
+            iterator = getItem(key);
+        } finally {
+            unlockRead();
+        }
+        
+        if (iterator == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public boolean remove(K key) {
+        
+        LockingParallelBucketList<T,K>.Iterator<T,K> iterator = head;
+        if (iterator == null) {
+          return false;
+        }
+
+        lockWrite();
+        try {
+            if (contains(key) == false) {
+                return false;      
+            }
+            if (head.key.equals(key)) {
+                head = head.getNext();
+                size--;
+                return true;
+            }
+            while (iterator.hasNext()) {
+                if (iterator.getNext().key.equals(key)) {
+                    iterator.setNext(iterator.getNext().getNext());
+                    size--;
+                    return true;
+                } else { 
+                    iterator = iterator.getNext();
+                }
+            }
+            return false;
+        } finally {
+            unlockWrite();
+        }
+    }
+    public void add(K key, T item) {
+        LockingParallelBucketList<T,K>.Iterator<T,K> iterator = head;
+
+        lockWrite();
+        try {
+            if (contains(key)) {
+                return;
+            } else {
+                SerialList<T,K>.Iterator<T,K> firstItem = new Iterator<T,K>(key, item, head);
+                head = firstItem;
+                size++;
+            }   
+        } finally {
+            unlockWrite();
+        }        
+    }
+  
+    public int getSize() {
+        return size;
+    }
+
+    
+
+    public void lockRead() {
+        lock.readLock().lock();
+    }
+    public void lockWrite() {
+        lock.writeLock().lock();   
+    }
+    public void unlockRead() {
+        lock.readLock().unlock();
+    }
+    public void unlockWrite() {
+        lock.writeLock().unlock();   
+    }
+
+    public class Iterator<T,K> {
+    @SuppressWarnings("unchecked")
+    public final K key;
+    private T item;
+    private Iterator<T,K> next;
+    public Iterator(K key, T item, Iterator<T,K> next) {
+      this.key = key;
+      this.item = item;
+      this.next = next;
+    }
+    @SuppressWarnings("unchecked")
+    public Iterator() {
+      this.key = (K) new Object();
+      this.item = (T) new Object();
+      this.next = null;
+    }
+    public boolean hasNext() {
+      return next != null;
+    }
+    @SuppressWarnings("unchecked")
+    public Iterator getNext() {
+      return next;
+    }
+    public void setNext(Iterator<T,K> next) {this.next = next; }
+    public T getItem() { return item; }
+    public void setItem(T item) { this.item = item; }
+  }
+
+
+}
 class SerialList<T,K> implements BucketList<T,K> {
   int size = 0;
   SerialList<T,K>.Iterator<T,K> head;
